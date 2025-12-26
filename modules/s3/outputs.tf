a78236a1ab33fd07fb0f5624e30e02d7864dd8fb
bhagirath1006 <bhagi.sy99@gmail.com>
2025-12-26 10:57:30 +0530

    second commit

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,25 +1,25 @@
 output "bucket_names" {
   description = "S3 bucket names"
   value = {
-    for key, bucket in aws_s3_bucket.main : key => bucket.id
+    for key, bucket in aws_s3_bucket.bucket : key => bucket.id
   }
 }
 
 output "bucket_arns" {
   description = "S3 bucket ARNs"
   value = {
-    for key, bucket in aws_s3_bucket.main : key => bucket.arn
+    for key, bucket in aws_s3_bucket.bucket : key => bucket.arn
   }
 }
 
 output "bucket_regions" {
   description = "S3 bucket regions"
   value = {
-    for key, bucket in aws_s3_bucket.main : key => bucket.region
+    for key, bucket in aws_s3_bucket.bucket : key => bucket.region
   }
 }
 
 output "bucket_name" {
   description = "First bucket name"
-  value       = try(values(aws_s3_bucket.main)[0].id, "")
+  value       = try(values(aws_s3_bucket.bucket)[0].id, "")
 }
